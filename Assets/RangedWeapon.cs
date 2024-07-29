c98b5914537e4e7c498011ccff808e6e73dfa19a
Sean Makoka <106635112+Terrasean123@users.noreply.github.com>
2024-07-29 17:00:36 +1000

    Aiming system for guns being worked on

--- Assets/RangedWeapon.cs
+++ Assets/RangedWeapon.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 
 public class RangedWeapon : MonoBehaviour
 {
+
+
     public enum GunType
     {
         sidearm,
@@ -16,45 +18,120 @@ public class RangedWeapon : MonoBehaviour
     }
 
     [Serializable]
-    public class GunVaribles
+    public class GunDetails
     {
         public GunType gunType;
         public Transform MuzzlePoint;
     }
-    public GunVaribles gunVaribles;
+    public GunDetails gunDetails;
 
     [SerializeField]
     GameObject CurrentlyHitting;
 
     LineRenderer lineRenderer;
+    LineRenderer cameraLineRenderer;
+
+    bool shouldDrawBulletTrail = false;
+
+    [SerializeField]
+    Camera camRef;
+
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        camRef = FindAnyObjectByType<Camera>();
+        cameraLineRenderer = camRef.GetComponent<LineRenderer>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         Ray ray = new Ray();
+        Ray cameraRay = new Ray();
 
-        ray.direction = gunVaribles.MuzzlePoint.transform.forward;
-        ray.origin = gunVaribles.MuzzlePoint.position;
+
+        //we get the start and direction for our "Bullet" from our Gun Here
+        ray.direction = gunDetails.MuzzlePoint.transform.forward;
+        ray.origin = gunDetails.MuzzlePoint.position;
+
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        RaycastHit cameraHit;
+
+        //GUN camera Interactions here 
+
+        cameraRay.origin = camRef.ScreenToWorldPoint(Vector3.zero);
+        cameraRay.direction = camRef.transform.forward;
+
+        if (Physics.Raycast(cameraRay, out cameraHit, Mathf.Infinity))
         {
-            //Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
-            lineRenderer.enabled = true;
-            CurrentlyHitting = hit.transform.gameObject;
+        }
+        cameraLineRenderer.SetPosition(0,cameraRay.origin);
+        cameraLineRenderer.SetPosition(1,cameraHit.point);
+
+
+        //Bullet visual Logic 
+        if (shouldDrawBulletTrail)
+        {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                lineRenderer.enabled = true;
+                CurrentlyHitting = hit.transform.gameObject;
+            }
+            else
+                lineRenderer.enabled = false;
+
+            lineRenderer.SetPosition(0, ray.origin);
+            lineRenderer.SetPosition(1, hit.point);
+
+
+            lineRenderer.SetPosition(2, ray.origin);
+            lineRenderer.SetPosition(3, hit.point + new Vector3(10,10,10));
         }
         else
             lineRenderer.enabled = false;
-         //   Debug.DrawRay(ray.origin, ray.direction, Color.yellow);
-
-
-        //Debug.Log(hit.point);
-        lineRenderer.SetPosition(0, ray.origin);
-        lineRenderer.SetPosition(1, hit.point);
     }
+
+    //active on beginning of Primary fire Action
+    public void OnPrimaryFireBegin()
+    {
+        shouldDrawBulletTrail = true;
+        Debug.Log("Beginning primary Fire");
+    }
+
+    //Active on Begining of alt-firing action
+    public void OnAltFireBegin()
+    {
+        shouldDrawBulletTrail = true;
+        Debug.Log("Beginnig alt Fire");
+    }
+
+    //Active every interval of Primaryfire set in this script
+    public void OnPrimaryFireStay()
+    {
+
+    }
+
+    //Active every interval  of altfire set in this script
+    public void OnAltFireStay()
+    {
+
+
+    }
+
+    //active on primary fire End
+    public void OnprimaryFireEnd()
+    {
+        shouldDrawBulletTrail = false;
+        Debug.Log("end Primary Fire");
+    }
+
+    //active on Alt-fire End
+    public void OnAltFireEnd()
+    {
+        shouldDrawBulletTrail = false;
+        Debug.Log("end alt fire");
+    }
+
 }
--- Assets/WeaponManagement.cs
+++ Assets/WeaponManagement.cs
@@ -6,55 +6,122 @@ using UnityEngine;
 public class WeaponManagement : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> weaponPrefabList;
+    List<RangedWeapon> weaponList;
 
     [SerializeField]
     int weaponIterator = 0;
 
-    [SerializeField]
-    GameObject activeWeapon;
 
     [SerializeField]
     Transform WeaponGripTransform;
+
+
+    [SerializeField]
+    RangedWeapon currentActiveWeapon;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        if (WeaponGripTransform.GetComponentInChildren<RangedWeapon>().gameObject)
-            activeWeapon = WeaponGripTransform.GetComponentInChildren<RangedWeapon>().gameObject;
+        SetWeapon(weaponIterator);
+
+        if (WeaponGripTransform.GetComponentInChildren<RangedWeapon>())
+        {
+            Debug.Log(WeaponGripTransform.GetComponentInChildren<RangedWeapon>().gameObject.name);
+        }
         else
             Debug.Log("no weapons found");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!activeWeapon || activeWeapon.GetComponent<RangedWeapon>().gunVaribles.gunType != weaponPrefabList[weaponIterator].GetComponent<RangedWeapon>().gunVaribles.gunType)
-        {
-            SetWeapon(weaponPrefabList[weaponIterator]);
-            Debug.Log("settingWeapon");
-        }
-
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             weaponIterator++;
-            weaponIterator %= weaponPrefabList.Count - 1;
-            if (weaponPrefabList[weaponIterator + 1])
+
+            if (weaponIterator < weaponList.Count)
             {
-                weaponIterator++;
+                Debug.Log("theres still weapons in this list ");
             }
-           
+            else
+            {
+                Debug.Log("at weapon List end");
+                weaponIterator %= weaponList.Count;
+                Debug.Log(weaponIterator);
+            }
+
+            SetWeapon(weaponIterator);
+            Debug.Log(weaponIterator);
         }
+
+        if (Input.GetMouseButton(0))
+        {
+            PrimaryFireWeapon();
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            currentActiveWeapon.OnprimaryFireEnd();
+
+
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            SecondaryFireWeapon();
+
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            currentActiveWeapon.OnAltFireEnd();
+        }
+
+
+    }
+    void PrimaryFireWeapon()
+    {
+        currentActiveWeapon.OnPrimaryFireBegin();
     }
 
-    void SetWeapon(GameObject weap)
+
+    void SecondaryFireWeapon()
     {
-        if (activeWeapon)
+        currentActiveWeapon.OnAltFireBegin();
+    }
+
+
+    //maybe
+    void WeaponListSet()
+    {
+
+
+    }
+
+    void SetWeapon(int weaponIndex)
+    {
+
+        //set previous to false
+        if (currentActiveWeapon)
+            currentActiveWeapon.gameObject.SetActive(false);
+
+        Debug.Log("weapon type " + weaponIndex);
+
+        currentActiveWeapon = weaponList[weaponIndex];
+        if (currentActiveWeapon)
         {
-            Destroy(activeWeapon);
+            Debug.Log("WeaponFound!!");
         }
-        activeWeapon = Instantiate(weap);
-        activeWeapon.transform.position = WeaponGripTransform.position;
-        activeWeapon.transform.rotation = WeaponGripTransform.rotation;
-        activeWeapon.transform.parent = WeaponGripTransform;
+
+        //set next to true 
+        currentActiveWeapon.gameObject.SetActive(true);
+
+        currentActiveWeapon.transform.position = WeaponGripTransform.position;
+        currentActiveWeapon.transform.rotation = WeaponGripTransform.rotation;
+        currentActiveWeapon.transform.parent = WeaponGripTransform;
     }
 }
