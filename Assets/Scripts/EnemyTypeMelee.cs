787ead86d8d8035bca4889f399108e5d38e2e1a3
sguiedi <85329147+sguiedi@users.noreply.github.com>
2024-08-05 11:41:35 +1000

    enemy health script, and starting working on new enemy scripts

---
+++ Assets/Scripts/EnemyTypeMelee.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTypeMelee : EnemyBase
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
