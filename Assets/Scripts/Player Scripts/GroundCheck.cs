15f299e856ff739cd8f36016825b1b9f9f210ced
AIE\s216146 <132528861+DerogeNaN@users.noreply.github.com>
2024-08-12 16:29:30 +1000

    Messed with ground check to get slope movement working, decided to avoid for now will implement later in development.
    TODO:
    Polish slide
    Setup Dash correctly
    Polish camera behaviour
    Start working on grapple mechanic

--- Assets/Scripts/Player Scripts/GroundCheck.cs
+++ Assets/Scripts/Player Scripts/GroundCheck.cs
@@ -6,6 +6,8 @@ public class GroundCheck : MonoBehaviour
     {
         //TODO: ADD GROUND LAYER SO THAT THE PLAYER CANT JUMP OFF ENEMIES
         Movement.playerMovement.isGrounded = true;
+        //Vector3 momentumDir = Vector3.Cross(other.transform.up, transform.right);
+        //Movement.playerMovement.momentum = momentumDir * Movement.playerMovement.momentum.magnitude;
         Movement.playerMovement.lastGroundedTime = Time.time;
         Movement.playerMovement.jumpCount = 0;
     }
@@ -14,6 +16,8 @@ public class GroundCheck : MonoBehaviour
     {
         //TODO: ADD GROUND LAYER SO THAT THE PLAYER CANT JUMP OFF ENEMIES
         Movement.playerMovement.isGrounded = true;
+        //Vector3 momentumDir = Vector3.Cross(other.transform.up, transform.right);
+        //Movement.playerMovement.momentum = momentumDir * Movement.playerMovement.momentum.magnitude;
         Movement.playerMovement.lastGroundedTime = Time.time;
         Movement.playerMovement.jumpCount = 0;
     }
