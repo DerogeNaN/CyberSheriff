c7bdfd33028bf54041a99c7ff19c4c8ffde3b552
Sean Makoka <106635112+Terrasean123@users.noreply.github.com>
2024-09-09 15:04:45 +1000

    revolver integrated into ranged weapon abstract class with bunch of fixes mixed in with it

--- Assets/Scripts/Revolver.cs
+++ Assets/Scripts/Revolver.cs
@@ -1,134 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEngine.AI;
-using UnityEngine.VFX;
-using TMPro;
-using System.Linq.Expressions;
-using UnityEditor.Rendering;
-using UnityEngineInternal;
-using System.Diagnostics.CodeAnalysis;
 
-public class Revolver : MonoBehaviour
+public class Revolver : RangedWeapon
 {
-
-    [Header("Gun Transforms")]
-
-    [SerializeField]
-    public Transform muzzlePoint;
-
-    [SerializeField]
-    public Transform altMuzzlePoint;
-
-    [Header("Gun Behaviour Values")]
-
-    [SerializeField]
-    [Tooltip("Ain't it self-explanatory?")]
-    int DamageValue = 25;
-
-    [SerializeField]
-    [Tooltip("A multiplier for the amount of force applied to an object  by the bullets")]
-    float bulletForceMultiplier = 1;
-
-    [Tooltip("Essentialy how long it takes to fire the next Bullet after one has already been fired")]
-    [SerializeField]
-    float shotGapTime = 1f;
-
-    [Tooltip("Essentialy how long it takes to fire the next Bullet after one has already been fired")]
-    [SerializeField]
-    float AltshotGapTime = 0.01f;
-
-    [Tooltip("How Long it takes to Refill The Clip")]
-    [SerializeField]
-    float reloadTime = 2f;
-
-    [SerializeField]
-    [Tooltip("How many bullets you can launch before reloading")]
-    public int BulletsPerClip = 6;
-
-    [SerializeField]
-    [Tooltip("Current Number of Bullets")]
-    public int currentBullets;
-
-    float BulletSpeed = 200;//obsolete till further notice
-
-    [Header("Gun Behaviour Bools (Do NOT mess With)")]
-
-    [SerializeField]
-    [Tooltip("Just a bool for whether Primary Fire should happen")]
-    bool shouldShootPrimary = false;
-
-    [SerializeField]
-    [Tooltip("Just a bool for if the secondary fire should happen ")]
-    bool shouldShootAlt = false;
-
-    [SerializeField]
-    bool canFire;
-
-    [SerializeField]
-    bool canPressAltFire = true;
-
-    [SerializeField]
-    bool waiting = false;
-
-    [SerializeField]
-    bool reloading;
-
-    [Header("Visual Effects")]
-    [SerializeField]
-    VisualEffect BulletFlash;
-
-    [SerializeField]
-    GameObject HitEffect;
-
-    [SerializeField]
-    GameObject BulletHitDecal;
-
-    [Header("Scene Refrences")]
-    [SerializeField]
-    Camera camRef;
-
-    [SerializeField]
-    public GameObject CurrentlyHitting;
-
-
-    public struct RayData
-    {
-        public Ray ray;
-        public RaycastHit hit;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        camRef = FindAnyObjectByType<Camera>();
-        currentBullets = BulletsPerClip;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (currentBullets <= 0 && reloading == false)
-        {
-            canFire = false;
-            StartCoroutine(Reload());
-        }
-
-        if (shouldShootPrimary == true && waiting == false && reloading == false)
-        {
-            EngagePrimaryFire();
-        }
-
-        if (shouldShootAlt == true && canPressAltFire == true && waiting == false && reloading == false)
-        {
-            EngageAltFire();
-        }
-    }
-
-    public void EngagePrimaryFire()
+    public override void EngagePrimaryFire()
     {
         //Primary Fire Logic
         if (currentBullets > 0)
@@ -142,12 +17,6 @@ public class Revolver : MonoBehaviour
 
                 if (rayData.hit.transform.gameObject.layer != 3) //If the thing hit isn't the player...
                 {
-                    if (!rayData.hit.transform.parent.GetComponentInChildren<EnemyTypeMelee>()) //AND it isn't an enemy
-                    {
-                        GameObject Decal = Instantiate(BulletHitDecal);
-                        Decal.transform.parent = rayData.hit.transform;
-                        Decal.transform.position = rayData.hit.point;
-                    }
                     //..It isn't the player but it is an enemy...?
                     GameObject hitFX = Instantiate(HitEffect);
                     hitFX.transform.position = rayData.hit.point;
@@ -160,10 +29,21 @@ public class Revolver : MonoBehaviour
                         Debug.Log("Does Not have rigidbody");
                     }
 
-                    if (rayData.hit.transform.parent.GetComponentInChildren<EnemyBase>())
+
+                    if (!rayData.hit.transform.parent && !rayData.hit.transform.TryGetComponent<EnemyBase>(out EnemyBase eb)) //AND it isn't an enemy
                     {
-                        Health EnemyHealth = rayData.hit.collider.transform.parent.GetComponentInChildren<Health>();
-                        EnemyHealth.TakeDamage(DamageValue, 0);
+                        GameObject Decal = Instantiate(BulletHitDecal);
+                        Decal.transform.parent = rayData.hit.transform;
+                        Decal.transform.position = rayData.hit.point;
+                    }
+
+                    if (rayData.hit.transform.parent)
+                    {
+                        if (rayData.hit.transform.parent.TryGetComponent<EnemyBase>(out EnemyBase eb2))
+                        {
+                            Health EnemyHealth = rayData.hit.collider.transform.parent.GetComponentInChildren<Health>();
+                            EnemyHealth.TakeDamage(DamageValue, 0);
+                        }
                     }
                 }
             }
@@ -178,7 +58,7 @@ public class Revolver : MonoBehaviour
     }
 
 
-    public void EngageAltFire()
+    public override void EngageAltFire()
     {
         //altFire Logic
         if (currentBullets > 0)
@@ -190,72 +70,6 @@ public class Revolver : MonoBehaviour
             StartCoroutine(Reload());
         }
     }
-
-
-    public RayData RayCastAndGenCameraRayData()
-    {
-        Ray cameraRay = new Ray();
-
-        RaycastHit cameraHit;
-
-        cameraRay.origin = camRef.ScreenToWorldPoint(Vector3.zero);
-
-        cameraRay.direction = camRef.transform.forward;
-
-        Physics.Raycast(cameraRay, out cameraHit, Mathf.Infinity);
-
-        return new RayData { ray = cameraRay, hit = cameraHit };
-    }
-
-    public RayData RayCastAndGenGunRayData(Transform muzzle)
-    {
-        Ray gunRay = new Ray();
-
-        //we get the start and direction for our "Bullet" from our Gun Here
-        gunRay.direction = muzzlePoint.transform.forward;
-        gunRay.origin = muzzlePoint.position;
-
-        RaycastHit gunHit;
-        RayData camRayData = RayCastAndGenCameraRayData();
-        //Here im getting the direction of a vector from the gun muzzle to reticle hit point 
-
-        Vector3 barrelToLookPointDir = camRayData.hit.point - muzzle.transform.position;
-
-        barrelToLookPointDir = math.normalize(barrelToLookPointDir);
-
-        //set ray direction to the barrel to look point direction 
-        gunRay.direction = barrelToLookPointDir;
-
-        Physics.Raycast(gunRay, out gunHit, Mathf.Infinity);
-
-        return new RayData { ray = gunRay, hit = gunHit };
-    }
-
-
-
-    //This coroutine  was made so the gun would wait for the shot gap time to pass before being able to fire again
-    IEnumerator Wait(float shotGapTime)
-    {
-        waiting = true;
-        yield return new WaitForSeconds(shotGapTime);
-        Debug.Log("Waiting...");
-        canFire = true;
-        waiting = false;
-    }
-
-    IEnumerator Reload()
-    {
-        reloading = true;
-        yield return new WaitForSeconds(reloadTime);
-        Debug.Log("Reloading...");
-        canFire = false;
-        if (currentBullets != BulletsPerClip)
-        {
-            currentBullets = BulletsPerClip;
-        }
-        reloading = false;
-    }
-
 
     IEnumerator FanFire()
     {
@@ -277,16 +91,9 @@ public class Revolver : MonoBehaviour
                 // bullet Hole Decal Placement Logic 
                 if (rayData.hit.transform.gameObject.layer != 3)
                 {
-                    if (!rayData.hit.transform.parent.GetComponentInChildren<EnemyTypeMelee>()) //AND it isn't an enemy
-                    {
-                        GameObject Decal = Instantiate(BulletHitDecal);
-                        Decal.transform.parent = rayData.hit.transform;
-                        Decal.transform.position = rayData.hit.point;
-                    }
-
+                    //..It isn't the player but it is an enemy...?
                     GameObject hitFX = Instantiate(HitEffect);
                     hitFX.transform.position = rayData.hit.point;
-
                     if (rayData.hit.rigidbody)
                     {
                         rayData.hit.rigidbody.AddForce(rayData.ray.direction * bulletForceMultiplier, ForceMode.Impulse);
@@ -296,10 +103,22 @@ public class Revolver : MonoBehaviour
                         Debug.Log("Does Not have rigidbody");
                     }
 
-                    if (rayData.hit.transform.parent.GetComponentInChildren<EnemyBase>())
+
+
+                    if (!rayData.hit.transform.parent && !rayData.hit.transform.TryGetComponent<EnemyBase>(out EnemyBase eb)) //AND it isn't an enemy
                     {
-                        Health EnemyHealth = rayData.hit.collider.transform.parent.GetComponentInChildren<Health>();
-                        EnemyHealth.TakeDamage(DamageValue, 0);
+                        GameObject Decal = Instantiate(BulletHitDecal);
+                        Decal.transform.parent = rayData.hit.transform;
+                        Decal.transform.position = rayData.hit.point;
+                    }
+
+                    if (rayData.hit.transform.parent)
+                    {
+                        if (rayData.hit.transform.parent.TryGetComponent<EnemyBase>(out EnemyBase eb2))
+                        {
+                            Health EnemyHealth = rayData.hit.collider.transform.parent.GetComponentInChildren<Health>();
+                            EnemyHealth.TakeDamage(DamageValue, 0);
+                        }
                     }
                 }
             }
@@ -312,21 +131,21 @@ public class Revolver : MonoBehaviour
     }
 
     //active on beginning of Primary fire Action
-    public void OnPrimaryFireBegin()
+    public override void OnPrimaryFireBegin()
     {
         shouldShootPrimary = true;
         Debug.Log("Beginning primary Fire");
     }
 
     //Active on Begining of alt-firing action
-    public void OnAltFireBegin()
+    public override void OnAltFireBegin()
     {
         shouldShootAlt = true;
         Debug.Log("Beginning primary Fire");
     }
 
     //Active every interval of Primaryfire set in this script
-    public void OnPrimaryFireStay()
+    public override void OnPrimaryFireStay()
     {
         if (shouldShootPrimary)
         {
@@ -335,7 +154,7 @@ public class Revolver : MonoBehaviour
     }
 
     //Active every interval  of altfire set in this script
-    public void OnAltFireStay()
+    public override void OnAltFireStay()
     {
         if (shouldShootAlt)
         {
@@ -345,14 +164,14 @@ public class Revolver : MonoBehaviour
     }
 
     //active on primary fire End
-    public void OnprimaryFireEnd()
+    public override void OnprimaryFireEnd()
     {
         shouldShootPrimary = false;
         Debug.Log("end Primary Fire");
     }
 
     //active on Alt-fire End
-    public void OnAltFireEnd()
+    public override void OnAltFireEnd()
     {
         shouldShootAlt = false;
         Debug.Log("end alt fire");
