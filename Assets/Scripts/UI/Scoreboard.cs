37328bed788791056df4725f79331db4a769dbd8
AIE\s230107 <s230107@aie.edu.au>
2024-10-14 17:14:51 +1100

    Started working on Scoreboard Recode (WIP)

--- Assets/Scripts/UI/Scoreboard.cs
+++ Assets/Scripts/UI/Scoreboard.cs
@@ -7,6 +7,7 @@ using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
+    [Header ("Scoreboard visuals")]
     public TMP_Text scoreText;
     public TMP_Text scoreShadowText;
     public TMP_Text rankText;
@@ -16,9 +17,43 @@ public class Scoreboard : MonoBehaviour
     public RawImage decayBar;
     public GameObject scoreboardPanel; // Reference to the scoreboard panel to show/hide
 
-    private int score;
-    private int comboMultiplier;
+    private float score;
+    private float comboMultiplier;
     private string rank;
+
+    //Internal Scorebar Progression
+    private int rankPointsMax = 100;
+    private int rankPointsCurrent = 0;
+
+    [Header("Rank up Balancing")] // these values control how much your decay bar jumps up when you score points.
+    [SerializeField] private int minorAction = 10;
+    [SerializeField] private int midAction = 20;
+    [SerializeField] private int majorAction = 50;
+
+    [Header("Score Bonus On Rank Decay")]
+    [SerializeField] private int eRankBonus = 5;
+    [SerializeField] private int dankBonus = 50;
+    [SerializeField] private int cRankBonus = 200;
+    [SerializeField] private int bRankBonus = 500;
+    [SerializeField] private int aRankBonus = 1000;
+    [SerializeField] private int sRankBonus = 2000;
+
+    [Header("Score Multipliers")]
+    [SerializeField] private float eRankMulti = 0.5f;
+    [SerializeField] private float dRankMulti = 1.0f;
+    [SerializeField] private float cRankMulti = 1.1f;
+    [SerializeField] private float bRankMulti = 1.3f;
+    [SerializeField] private float aRankMulti = 1.6f;
+    [SerializeField] private float sRankMulti = 1.7f;
+
+    [Header("Rank Handycap")]
+    [SerializeField] private float eRankHandCap = 1.0f;
+    [SerializeField] private float dRankHandCap = 0.9f;
+    [SerializeField] private float cRankHandCap = 0.8f;
+    [SerializeField] private float bRankHandCap = 0.7f;
+    [SerializeField] private float aRankHandCap = 0.6f;
+    [SerializeField] private float sRankHandCap = 0.5f;
+
 
     private float decayTimer;
     public float decayInterval = 10f; // Time in seconds before rank decays
@@ -110,25 +145,21 @@ public class Scoreboard : MonoBehaviour
         UpdateScoreboard();
     }
 
-    public void IncrementCombo()
+    public void IncrementMuiltiplier()
     {
         comboMultiplier++;
         UpdateScoreboard();
     }
 
-    public void ResetCombo()
+    public void ResetMuiltiplier()
     {
-        comboMultiplier = 1;
+        comboMultiplier = eRankMulti;
         UpdateScoreboard();
     }
     public void ResetScore()
     {
         score = 0;
         UpdateScoreboard();
-    }
-    public void ScoreDeduction()
-    {
-        score = score/2;
     }
 
 
@@ -161,41 +192,40 @@ public class Scoreboard : MonoBehaviour
         {
             case "S":
                 rank = "A";
-                ScoreDeduction();
                 scoreTextShakeEffect.StartScorePulsateAndShake();
                 Debug.Log("S to A");
                 break;
             case "A":
                 rank = "B";
-                ScoreDeduction();
+
                 scoreTextShakeEffect.StartScorePulsateAndShake();
                 Debug.Log("A to B");
                 break;
             case "B":
                 rank = "C";
-                ScoreDeduction();
+
                 scoreTextShakeEffect.StartScorePulsateAndShake();
                 Debug.Log("B to C");
                 break;
             case "C":
                 rank = "D";
-                ScoreDeduction();
+
                 scoreTextShakeEffect.StartScorePulsateAndShake();
                 Debug.Log("C to D");
                 break;
             case "D":
                 rank = "E";
-                ScoreDeduction();
+
                 scoreTextShakeEffect.StartScorePulsateAndShake();
                 Debug.Log("D to E");
                 break;
             case "E":
-                ResetScore();
+                //ResetScore();
                 Debug.Log("Reset Score");
                 break;
                 
         }
-        ResetCombo();
+        ResetMuiltiplier();
         //ScoreDeduction();
         //scoreTextShakeEffect.StartScorePulsateAndShake();
         UpdateScoreboard();
