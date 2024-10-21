ef13a2e9f585c31f6757c3c133da495e0043a9f6
sguiedi <85329147+sguiedi@users.noreply.github.com>
2024-10-21 16:58:47 +1100

    flying enemy avoiding other enemies (wip)

--- Assets/Scripts/AI Scripts/EnemyFlying.cs
+++ Assets/Scripts/AI Scripts/EnemyFlying.cs
@@ -1,6 +1,7 @@
 using Autodesk.Fbx;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Drawing;
 using UnityEngine;
 
 public class EnemyFlying : EnemyBase
@@ -20,6 +21,7 @@ public class EnemyFlying : EnemyBase
     public float avoidDistance;
     public float avoidanceDistanceWeight;
     public float avoidanceSpeedWeight;
+    public float avoidFloorDistance;
 
     Vector3 dir;
     Vector3 toPlayer;
@@ -64,7 +66,7 @@ public class EnemyFlying : EnemyBase
         Vector3 towardsPlayer = currentSpeed * dir;
 
         // apply movement
-        transform.position += towardsPlayer + GetAvoidance();
+        transform.position += towardsPlayer + GetAvoidance2();
     }
 
     private Vector3 GetAvoidance()
@@ -72,25 +74,64 @@ public class EnemyFlying : EnemyBase
         Vector3 avoidance = new();
         Vector3[] rays =
         {
-             Quaternion.Euler(0, -45, 0) * transform.forward, // left
-             Quaternion.Euler(0, 45, 0) * transform.forward, // right
+             Quaternion.Euler(0, -45, 0) * transform.forward * avoidDistance, // left
+             Quaternion.Euler(0, 45, 0) * transform.forward * avoidDistance,  // right
+             new Vector3(0.0f, -1.0f, 0.0f) * avoidFloorDistance,             // down
+             //transform.forward * avoidFloorDistance,                        // forward
         };
 
         // loop through rays for each direction and add to avoidance amount
         foreach (Vector3 v in rays)
         {
-            Debug.DrawRay(transform.position, v * avoidDistance);
+            Debug.DrawRay(transform.position, v);
 
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, v, out hit, avoidDistance))
+            if (Physics.Raycast(transform.position, v, out hit, v.magnitude))
             {
                 avoidance += hit.normal * (hit.distance * avoidanceDistanceWeight);
             }
         }
 
         // scale avoidance based on the percentage of current speed to max speed (i think)
-        avoidance *= ((currentSpeed / maxSpeed) * avoidanceSpeedWeight);
+        //avoidance *= ((currentSpeed / maxSpeed) * avoidanceSpeedWeight);
 
         return avoidance * avoidanceStrength;
+    }
+
+    private Vector3 GetAvoidance2()
+    {
+        Vector3 avoidance = new();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, avoidDistance, LayerMask.GetMask("Enemy"));
+
+        foreach (Collider c in colliders)
+        {
+            if (!IsDescendantOf(transform, c.gameObject.transform) && c.gameObject.transform != transform)
+            {
+                Debug.Log(c.gameObject.name);
+                Vector3 fromCollision = transform.position - c.transform.position;
+                avoidance += fromCollision.normalized * (fromCollision.magnitude * avoidanceDistanceWeight);
+            }
+        }
+
+        return avoidance * avoidanceStrength;
+    }
+
+    static bool IsDescendantOf(Transform maybeParent, Transform maybeChild)
+    {
+        if (maybeParent == maybeChild.parent) return true;
+        if (maybeChild.parent == null) return false;
+        return IsDescendantOf(maybeParent, maybeChild.parent);
+    }
+    static bool IsDescendantOf_NotRecursive(Transform maybeParent, Transform maybeChild)
+    {
+        Transform currentChild = maybeChild;
+
+        while (true)
+        {
+            if (currentChild.parent == null) return false;
+            if (currentChild.parent == maybeParent) return true;
+            currentChild = currentChild.parent;
+        }
     }
 }
