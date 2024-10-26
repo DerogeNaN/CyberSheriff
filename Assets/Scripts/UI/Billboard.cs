17ee00115704da3be7c8eecd84a985e38def6f2b
ArchMoff <s221368@students.aie.edu.au>
2024-10-26 14:19:08 +1100

    Fixed explosion, added grapple ui stuff

---
+++ Assets/Scripts/UI/Billboard.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+    [SerializeField] private BillboardType billboardType;
+    
+    [Header("Lock Rotation")]
+    [SerializeField] private bool lockX;
+    [SerializeField] private bool lockY;
+    [SerializeField] private bool lockZ;
+
+    private Vector3 originalRotation;
+
+    public enum BillboardType { LookAtCamera, CameraForward };
+
+  void LateUpdate() {
+        switch (billboardType) {
+            case BillboardType.LookAtCamera:
+                transform.LookAt(Camera.main.transform.position, Vector3.up);
+                break;
+            case BillboardType.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+              default:
+                break;
+        }
+
+        Vector3 rotation = transform.rotation.eulerAngles;
+        if (lockX) { rotation.x = originalRotation.x; }
+        if (lockY) { rotation.y = originalRotation.y; }
+        if (lockZ) { rotation.z = originalRotation.z; }
+        transform.rotation = Quaternion.Euler(rotation);
+   }
+}
